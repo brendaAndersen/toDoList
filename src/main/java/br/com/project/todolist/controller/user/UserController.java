9e033c483c0fe962da856193ef296174ea8931c1
brenda <brenda_andersen@protonmail.com>
2024-08-14 22:46:50 -0300

    added configs

--- src/main/java/br/com/project/todolist/controller/user/UserController.java
+++ src/main/java/br/com/project/todolist/controller/user/UserController.java
@@ -1,7 +1,16 @@
-package br.com.project.todolist.controller;
+package br.com.project.todolist.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel);
+    }
 
 }
